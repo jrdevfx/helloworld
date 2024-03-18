406d10c7bb1779469eb3c901e744b3f7bfeeeac5
jrdevfx <140286013+jrdevfx@users.noreply.github.com>
2024-03-18 21:21:35 +0000

    Update main.tf
    add sleep 300 seconds

--- main.tf
+++ main.tf
@@ -19,3 +19,8 @@ resource "random_id" "id" {
 output "hello_world" {
   value = "Hello World, ${var.subject} ${random_id.id.hex}!"
 }
+
+
+resource "time_sleep" "wait_300_seconds" {
+  create_duration = "300s"
+}
